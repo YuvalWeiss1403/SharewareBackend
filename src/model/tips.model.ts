96d91043976ac0bfab8a5d3a34909117dd5a3ca7
HagarRam <119937814+HagarRam@users.noreply.github.com>
2023-03-23 16:09:34 +0200

    with likes!

--- src/model/tips.model.ts
+++ src/model/tips.model.ts
@@ -4,6 +4,7 @@ export interface ITips {
 	title: string;
 	username: string;
 	approved?: boolean;
+	likes: number;
 	comment: string;
 }
 
@@ -11,6 +12,7 @@ export const tipsSchema = new Schema<ITips>({
 	title: { type: String, required: true },
 	username: { type: String, required: true },
 	comment: { type: String, required: true },
+	likes: { type: Number, required: true },
 	approved: { type: Boolean, required: false },
 });
 
